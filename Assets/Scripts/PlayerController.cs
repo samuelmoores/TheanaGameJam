dbd54086689014d33543431a445aa54834d77c9c
samuelmoores <samueldmoores@gmail.com>
2024-04-07 15:20:11 -0700

    player movement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,15 +4,36 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public float playerSpeed;
+    public float rotationSpeed;
+
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("HelloWorld");
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float verticalInput = Input.GetAxisRaw("Vertical");
+        float horizontalInput = Input.GetAxisRaw("Horizontal");
+
+        Vector3 movementDirection = new Vector3(horizontalInput, 0, verticalInput);
+        movementDirection.Normalize();
+
+
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(movementDirection * playerSpeed * Time.deltaTime, Space.World);
+     
+        }
+
+        if(movementDirection != Vector3.zero)
+        {
+            Quaternion newRotation = Quaternion.LookRotation(movementDirection, Vector3.up);
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, newRotation, rotationSpeed * Time.deltaTime);
+        }
+
     }
 }
